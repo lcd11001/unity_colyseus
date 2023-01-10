0b558c69f2aceb45d8d5cda460e2a216a6380f26
GAMELOFT\dan.luongcong <dan.luongcong@gameloft.com>
2023-01-10 15:30:37 +0700

    + Ai Controller

---
+++ Assets/PongGame/Scripts/AIController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PaddleController))]
+public class AIController : MonoBehaviour
+{
+	[SerializeField] private BallController ball;
+	[SerializeField] private float timeInterval = 1.0f;
+
+	private float time = 0;
+	private PaddleController paddle;
+	private float targetPositionX;
+
+	private void Start()
+	{
+		paddle = GetComponent<PaddleController>();
+	}
+
+	void Update()
+    {
+		time += Time.deltaTime;
+		if (time > timeInterval)
+		{
+			targetPositionX = ball.transform.position.x;
+			time = 0;
+		}
+
+		if (paddle.transform.position.x != targetPositionX)
+		{
+			paddle.MoveTo(targetPositionX);
+		}
+	}
+}
--- Assets/PongGame/Scripts/PaddleController.cs
+++ Assets/PongGame/Scripts/PaddleController.cs
@@ -8,11 +8,14 @@ public class PaddleController : MonoBehaviour
 {
 	[SerializeField] float moveSpeed = 10f;
 	[SerializeField] float limitX = 6.9f;
+	[SerializeField] float touchControlDistance = 5f;
 	private Rigidbody rb;
+	private float distanceToCamera;
 
 	private void Start()
 	{
 		rb = GetComponent<Rigidbody>();
+		distanceToCamera = Vector3.Distance(transform.position, Camera.main.transform.position);
 	}
 
 	private void Update()
@@ -22,15 +25,43 @@ public class PaddleController : MonoBehaviour
 
 	private void Movement()
 	{
+		Vector3 myPosition = rb.position;
+
 		if (Input.GetMouseButton(0))
 		{
-			Vector3 mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.y));
-			Vector3 myPosition = rb.position;
-
-			myPosition.x = Mathf.Lerp(myPosition.x, mousePosition.x, moveSpeed);
-			myPosition.x = Mathf.Clamp(myPosition.x, -limitX, limitX);
-
-			rb.position = myPosition;
+			Vector3 mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distanceToCamera));
+			if (Mathf.Abs(mousePosition.z - myPosition.z) <= touchControlDistance)
+			{
+				MoveTo(mousePosition.x);
+			}
+			//else
+			//{
+			//	Debug.Log($"mouse {mousePosition.z} vs {rb.gameObject.name} {myPosition.z}");
+			//}
 		}
+
+		if (Input.touchCount > 0)
+		{
+			Touch touch = Input.touches[0];
+			Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+			if (Mathf.Abs(touchPosition.z - myPosition.z) <= touchControlDistance)
+			{
+				MoveTo(touchPosition.x);
+			}
+			//else
+			//{
+			//	Debug.Log($"touch {touchPosition.z} vs {rb.gameObject.name} {myPosition.z}");
+			//}
+		}
+	}
+
+	public void MoveTo(float x)
+	{
+		Vector3 myPosition = rb.position;
+
+		myPosition.x = Mathf.Lerp(myPosition.x, x, moveSpeed * Time.deltaTime);
+		myPosition.x = Mathf.Clamp(myPosition.x, -limitX, limitX);
+
+		rb.position = myPosition;
 	}
 }
